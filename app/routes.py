1537e4eec0bc3f6533444f9b93ebeccbf2551ab7
Zoë Wilkinson Saldaña <zoews@umich.edu>
2018-05-28 15:47:40 -0400

    Up to microblog step

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return "Hello, World!"
+	user = {'username': 'Doggo'}
+	posts = [
+		{
+			'author': {'username': 'Binta'},
+			'body': 'Beautiful day in Ithaca!'
+		},
+		{
+			'author': {'username': 'Milo'},
+			'body': 'The Avengers movie was so not so great!'
+		}
+	]
+	return render_template('index.html', title="Home", user=user, posts=posts)
